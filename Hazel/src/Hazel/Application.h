418a236b912ee74de2df7a3218235e97e2131642
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-07-23 22:45:10 -0300

    22:44-07/23/2024: The app worked fine but I didn't have Hazel.ilk in my Hazel.

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Hazel 
+{ 
+	class __declspec(dllexport) Application
+	{
+	public:
+		Application();
+
+		virtual ~Application();
+
+		void Run();
+	};
+
+	// To be defined in CLIENT
+	Application* CreateApplication();
+}
